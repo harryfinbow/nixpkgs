af692218364017586094864cf3d5c630a70a9a1c
Harry Finbow <harry@finbow.dev>
2025-05-30 15:45:47 +0100

    buildbox: 1.3.11 -> 1.3.19

--- pkgs/by-name/bu/buildbox/package.nix
+++ pkgs/by-name/bu/buildbox/package.nix
@@ -21,13 +21,13 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "buildbox";
-  version = "1.3.11";
+  version = "1.3.19";
 
   src = fetchFromGitLab {
     owner = "BuildGrid";
     repo = "buildbox/buildbox";
     tag = finalAttrs.version;
-    hash = "sha256-lIRYwZLjYCpA4TMO3GF/yykVKn7LDyNHW9zItZmS9vM=";
+    hash = "sha256-F3zTortkDYF13fkxAVWbQlaysZnGbOdcp/czbthXAKw=";
   };
 
   nativeBuildInputs = [
